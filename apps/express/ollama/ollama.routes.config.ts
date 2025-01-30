569279a7fddb4b5ab141c8014382cfd8362c291d
bluefits <44682470+BlueFits@users.noreply.github.com>
2025-01-29 19:54:47 -0500

    Update ollama.routes.config.ts

--- apps/express/ollama/ollama.routes.config.ts
+++ apps/express/ollama/ollama.routes.config.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { OllamaController } from './ollama.controller';
 import { CommonRoutesConfig } from '../common/common.routes.config';
-import { ElevenLabsClient, play } from "elevenlabs";
 
 export class OllamaRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
